5a5d964c0ebe93caa6bc9966a9985fe4b7b22939
Programmer100th <48069809+Programmer100th@users.noreply.github.com>
2020-05-23 15:43:13 +0200

    Aggiunto file listOfTitles
    Preso in input un csv produce in output una lista contenente i titoli delle pagine

---
+++ listOfTitles.py
@@ -0,0 +1,24 @@
+def returnTitles(filename):
+    blacklist = ['/', '&']
+    fin = open(filename, "r", encoding = "utf-8")
+    listOfTitles = []
+    firstRow = fin.readline()
+    for row in fin:
+        row = row.strip().split(';')
+        title = row[0]
+        incomingLinks = row[1]
+        if(checkTitle(title, blacklist)):
+            listOfTitles.append(title)
+    
+    return listOfTitles
+
+
+def checkTitle(title, blacklist):
+    for char in title:
+        for elem in blacklist:
+            if(char == elem):
+                return False
+    return True
+
+myList = returnTitles("dataset.csv")
+    
